80a7bcafc38d237d956cc3335e80779f7141ee28
Ernesto Zanza <prvtezachscr2.sgn@gmail.com>
2025-07-13 17:53:42 -0400

    Modified: added email regex into form.js

--- form.js
+++ form.js
@@ -1,28 +1,47 @@
 const btn = document.getElementById("send-email");
 const form = document.getElementById("contact-form");
+const emailError = document.getElementById("email-error");
+const emailInput = form.querySelector('input[type="email"]');
+const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/im;
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+function validateEmail() {
+  const emailValue = form.email.value.trim();
+
+  if (!emailRegex.test(emailValue)) {
+    emailError.hidden = false;
+    emailInput.focus();
+    return false;
+  } else {
+    emailError.hidden = true;
+    return true;
+  }
+}
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  if (!validateEmail()) {
+    return
+  }
 
   // button animation while sending
+  btn.disabled = true;
   btn.classList.toggle('blink-pulsate');
   btn.innerHTML = "Enviando correo...";
 
   const serviceID = "default_service";
   const templateID = "template_yycxo07";
-
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
       btn.innerHTML = "Enviar correo";
       alert("Tu email ha sido enviado exitosamente!");
       btn.classList.toggle('blink-pulsate');
-
       form.reset()
-    },
-    (err) => {
+      btn.disabled = false;
+    }).catch((err) => {
       btn.innerHTML = "Enviar correo";
       alert(`Tu email no ha podido ser enviado :( \n Error: ${JSON.stringify(err)}`);
       btn.classList.toggle('blink-pulsate');
-    }
-  );
+      btn.disabled = false;
+    });
 });
